6d10261b8a0ed9567c9d26ac7aaa8fbab8de17aa
Vadym Shubkin <vadym.shubkin@gmail.com>
2024-12-26 12:46:18 +0200

    Update first.groovy

--- vars/first.groovy
+++ vars/first.groovy
@@ -1,3 +1,9 @@
 def call(Map map){
   echo "Hey ${map.name}, here I am, age of ${map.age}"
+  def newAge = calcul(map.age)
+  echo "Hey ${map.name}, here I am, age of ${newAge}"
+}
+
+def int calcul(int a){
+  return a+a
 }
